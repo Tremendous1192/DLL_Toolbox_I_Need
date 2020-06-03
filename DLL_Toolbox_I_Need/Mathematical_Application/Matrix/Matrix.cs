cad19dd9259686bdcf9eeba88374d3ce5e4a3484
Tremendous1192 <tremendous.duck@gmail.com>
2020-06-03 19:49:17 +0900

    namespace Mathmatical_Applicationを追加。 class Matrixを追加。

---
+++ DLL_Toolbox_I_Need/Mathematical_Application/Matrix/Matrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL_Toolbox_I_Need.Mathematical_Application
+{
+    public partial class Matrix
+    {
+    }
+}
