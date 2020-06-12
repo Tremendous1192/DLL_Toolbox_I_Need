18e81cc929056866c2744ec1120ba5f1b5256e82
Tremendous1192 <tremendous.duck@gmail.com>
2020-06-13 07:21:10 +0900

    Design_matrix classの分散共分散行列の計算式を変更。定義通りから、E[XY]-E[X]E[Y]に変更。

--- DLL_Toolbox_I_Need/Mathematical_Application/Design_Matrix/Design_Matrix_V.cs
+++ DLL_Toolbox_I_Need/Mathematical_Application/Design_Matrix/Design_Matrix_V.cs
@@ -19,6 +19,7 @@ namespace DLL_Toolbox_I_Need.Mathematical_Application
         public static double[,] Variance_Covariance_Matrix(double[,] design_Matrix)
         {
 
+            /*
             double[,] average = Design_Matrix.Average(design_Matrix);
 
             double[,] variance_Covariance_Matrix
@@ -31,7 +32,7 @@ namespace DLL_Toolbox_I_Need.Mathematical_Application
             for (int i = 0; i < design_Matrix.GetLength(0); i++)
             {
                 //i行目のベクトルと平均値ベクトルとの差分ベクトルを求め、j列目とk列目との積を、共分散行列の要素[j,k]に加える。
-                for (int j = 0; j < row_Vector.GetLength(1); j++) 
+                for (int j = 0; j < row_Vector.GetLength(1); j++)
                 {
                     row_Vector[0, j] = design_Matrix[i, j] - average[0, j];
                 }
@@ -60,8 +61,38 @@ namespace DLL_Toolbox_I_Need.Mathematical_Application
                 }
             }
 
+            return variance_Covariance_Matrix;
+            */
+
+
+            double[,] average = Design_Matrix.Average(design_Matrix);
+
+            double[,] variance_Covariance_Matrix
+                     = new double[design_Matrix.GetLength(1), design_Matrix.GetLength(1)];
+
+            //分散・共分散行列の要素[j,k]を計算する。
+            for (int j = 0; j < design_Matrix.GetLength(1); j++)
+            {
+                for (int k = j; k < design_Matrix.GetLength(1); k++)
+                {
+
+                    //j次元目とk次元目の積の平均値を計算する。 E[XY]
+                    for (int n = 0; n < design_Matrix.GetLength(0); n++)
+                    {
+                        variance_Covariance_Matrix[j, k] += design_Matrix[n, j] * design_Matrix[n, k];
+                    }
+                    variance_Covariance_Matrix[j, k] /= design_Matrix.GetLength(0);
+
+                    //j次元目の平均値とk次元目の平均値の積を引く。-E[X]E[Y]
+                    variance_Covariance_Matrix[j, k] -= average[0, j] * average[0, k];
+                    
+                    //j , kを入れ替えても値は同じ
+                    variance_Covariance_Matrix[k, j] = variance_Covariance_Matrix[j, k];
+                }
+            }
 
             return variance_Covariance_Matrix;
+
         }
 
 
