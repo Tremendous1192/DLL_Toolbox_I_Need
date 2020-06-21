ee2ebcc5a4a0204322d0bc3a71fb7813224ddada
Tremendous1192 <tremendous.duck@gmail.com>
2020-06-21 17:38:06 +0900

    Matrix classの行列式の計算methodを修正しました。逆行列の計算で使用した正則化項を削除しました。逆行列の有無を確認するには、この項は必要ないからです。

--- DLL_Toolbox_I_Need/Demonstration/My_class/My_class_002.cs
+++ DLL_Toolbox_I_Need/Demonstration/My_class/My_class_002.cs
@@ -38,51 +38,62 @@ namespace DLL_Toolbox_I_Need.Demonstration
 
             Console.ReadKey();
 
-
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine("\n行列A");
+            this.Show_Matrix_Element(matrix_A);
             Console.WriteLine("\n行列の、各要素の絶対値の和を得ます。");
             double scalar = Matrix.Norm_L1(matrix_A);
             Console.WriteLine(scalar);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("\n行列の、各要素の二乗和の平方根を得ます。");
             scalar = Matrix.Norm_L2(matrix_A);
             Console.WriteLine(scalar);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("\n行列の、各要素の絶対値の最大値を得ます。");
             scalar = Matrix.Norm_Infinity(matrix_A);
             Console.WriteLine(scalar);
             Console.ReadKey();
 
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine("\n行列A");
+            this.Show_Matrix_Element(matrix_A);
             Console.WriteLine("\n行列Aの第0行を取り出して、行ベクトルを得ます");
             double[,] result = Matrix.Pick_Up_Row_Vector(matrix_A, 0);
             this.Show_Matrix_Element(result);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("\n行列Aの第0列を取り出して、列ベクトルを得ます");
             result = Matrix.Pick_Up_Column_Vector(matrix_A, 0);
             this.Show_Matrix_Element(result);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("\n行列Aの第1行から1行、第0列から2列を取り出して、部分行列を得ます");
             result = Matrix.SubMatrix(matrix_A, 1, 1, 0, 2);
             this.Show_Matrix_Element(result);
             Console.ReadKey();
 
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine("\n行列B");
+            this.Show_Matrix_Element(matrix_B);
             Console.WriteLine("\n正方行列の跡を得ます。");
             scalar = Matrix.Trace(matrix_B);
             Console.WriteLine(scalar);
             Console.ReadKey();
 
+            Console.WriteLine("\n\n\n\n");
+            Console.WriteLine("\n行列B");
+            this.Show_Matrix_Element(matrix_B);
             Console.WriteLine("\n正方行列の行列式を得ます。");
             scalar = Matrix.Determinant(matrix_B);
             Console.WriteLine(scalar);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("\n正方行列の逆行列を得ます。");
             result = Matrix.Inverse_of_a_Matrix(matrix_B);
             this.Show_Matrix_Element(result);
-            Console.ReadKey();
+            //Console.ReadKey();
 
             Console.WriteLine("\n逆行列と、元の行列の積は単位行列になることを確認します。");
             result = Matrix.Multiplication(matrix_B, Matrix.Inverse_of_a_Matrix(matrix_B));
